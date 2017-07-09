41a4283db7d76e977e1d2af387464140b6a67fd0
Javier Pollak <javi.po.123@gmail.com>
2017-07-09 17:40:24 -0400

    Fix punctuation in comments

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
     "sync"
 )
 
-// Session holds the connection to the server
+// Session holds the connection to the server.
 type Session struct {
     connection net.Conn
     sendingCommand *sync.Mutex
 }
 
-// Connect to the jamyxer server
+// Connect to the jamyxer server.
 func (session* Session) Connect(ip string, port int) {
     conn, err := net.Dial("tcp", ip+":"+strconv.Itoa(port))
     if err != nil {
@@ -25,7 +25,7 @@ func (session* Session) Connect(ip string, port int) {
     session.sendingCommand = &sync.Mutex{}
 }
 
-// Send a command to the jamyxer server
+// Send a command to the jamyxer server.
 func (session* Session) SendCommand(cmd string, a ...interface{}) (reply string) {
     cmd_s := fmt.Sprintf(cmd, a...)
 
@@ -51,46 +51,46 @@ func (session* Session) SendCommand(cmd string, a ...interface{}) (reply string)
     return string(reply_b[:numbytes])
 }
 
-// Returns an array of strings representing the names of the input channels
+// Returns an array of strings representing the names of the input channels.
 func (session *Session) GetInputs() []string {
     reply := strings.Trim(session.SendCommand("gi\n"), "\n")
     return strings.Split(reply, "\n")
 }
 
-// Set volume for specified input channel
+// Set volume for specified input channel.
 func (session *Session) VolumeInputSet(input string, volume float64) {
     session.SendCommand("vis \"%s\" %f\n", input, volume)
 }
 
-// Set volume for specified output channel
+// Set volume for specified output channel.
 func (session *Session) VolumeOutputSet(output string, volume float64) {
     session.SendCommand("vos \"%s\" %f\n", output, volume)
 }
 
-// Get volume for specified input channel
+// Get volume for specified input channel.
 func (session *Session) VolumeInputGet(input string) float64 {
     vol_s := session.SendCommand("vig \"%s\"\n", input)
     vol, _ := strconv.ParseFloat(vol_s, 64)
     return vol
 }
 
-// Get volume for specified output channel
+// Get volume for specified output channel.
 func (session *Session) VolumeOutputGet(output string) float64 {
     vol_s := session.SendCommand("vog \"%s\"\n", output)
     vol, _ := strconv.ParseFloat(vol_s, 64)
     return vol
 }
 
-// Listen for volume for specified input channel
-// This is a blocking call waiting for a change in volume and returning it
+// Listen for volume for specified input channel.
+// This is a blocking call waiting for a change in volume and returning it.
 func (session *Session) VolumeInputListen(input string) float64 {
     vol_s := session.SendCommand("listni \"%s\"\n", input)
     vol, _ := strconv.ParseFloat(vol_s, 64)
     return vol
 }
 
-// Listen for volume for specified output channel
-// This is a blocking call waiting for a change in volume and returning it
+// Listen for volume for specified output channel.
+// This is a blocking call waiting for a change in volume and returning it.
 func (session *Session) VolumeOutputListen(output string) float64 {
     vol_s := session.SendCommand("listno \"%s\"\n", output)
     vol, _ := strconv.ParseFloat(vol_s, 64)
